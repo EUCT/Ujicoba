32bf17d520bfebdba3183a4d4a8edc5175cd9b9d
EUCT <57327818+EUCT@users.noreply.github.com>
2019-11-19 15:10:22 +0700

    Add files via upload

---
+++ Ujicoba.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+#version 1.0
+clear
+# Variables
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red="\e[91m"
+green="\033[0;32m"
+light="\e[92m"
+echo ""
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo "\033[32;1mAuthor : Mr.CR45H"
+echo "\033[37;1mFeat: Mr.ZhuSyren7"
+echo "\033[35;1mversion Tools: 1"
+echo "\033[35;1mTeam : Lolzecure "
+echo "\033[33;1mKontak Me :  085755365813"
+echo "\033[92mExploit Union Cyber Team" 
+echo "\033[36;1mLolzecure"
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo "[===============================================[>"
+echo $green "1.> Hack Facebook"
+echo "[===============================================]>"
+echo $purple "2.> DDOS Trojan"
+echo "[===============================================]>"
+echo $cyan "3.> Sadap WhatsApp"
+echo "[===============================================]>"
+echo $red "4.> Spam Sms HooqTV"
+echo "[===============================================]>"
+echo $white "5.> Spam Sms KFC"
+echo "[===============================================]>"
+echo $green "6.> Spam Sms PHD"
+echo "[===============================================]>"
+echo $purple "7.> Spam Sms Whiskas"
+echo "[===============================================]>"
+echo $cyan "8.> Spam Sms ZiPAY"
+echo "[===============================================]>"
+echo $red "9.> Nyari CC buat Carding"
+echo "[===============================================]>"
+echo $white "10.> Spam Akun Gmail"
+echo "[===============================================]>"
+echo $green "11.> Whois LookUp"
+echo "[===============================================]>"
+echo $purple "12.> Upload Admin Finder"
+echo "[===============================================]>"
+echo $cyan "13.> Script Deface Creator"
+echo "[===============================================]>"
+echo $red "14.> ReConDog Buat Information gathering"
+echo "[===============================================]>"
+echo $white "15.> Admin Login Finder"
+echo "[===============================================]>"
+echo $green "16.> Virus VbugMaker Creator"
+echo "[===============================================]>"
+echo $purple "17.> dowload script deface orang"
+echo "[===============================================]>"
+echo $cyan "18.> Autolike Facebook"
+echo "[===============================================]>"
+echo $red "19.> Facebook Auto_Reaction"
+echo "[===============================================]>"
+echo $white "20.> Bot Komentar Facebook"
+echo "[===============================================]>"
+echo $green "21.> Install txtool"
+echo "[===============================================]>"
+echo $purple "22.> install sqlscan"
+echo "[===============================================]>"
+echo $cyan "23.> install hash-buster"
+echo "[===============================================]>"
+echo $red "24.> Install Red_Hawk"
+echo "[===============================================]>"
+echo $white "25.> Install Torshammer buat DDOS website"
+echo "[===============================================]>"
+echo $green "26.> Install LITEDDOS buat DDOS website"
+echo "[===============================================]>"
+echo $purple "27.> Install Hunner Framework"
+echo "[===============================================]>"
+echo $cyan "28.> Install Sqlmap Buat Inject Situs"
+echo "[===============================================]>"
+echo $red "29.> Install 4wsectools"
+echo "[===============================================]>"
+echo $white "30.> Install Lazymux Buat Install Tools"
+echo "[===============================================]>"
+echo $yellow "0.> keluar"
+echo "[+]===============================================[+]"
+echo "\033[32;1m"
+read -p "root@T00Ls-CR45Hv1~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet "Mr.CR45H" 
+https://github.com/pirmansx/mbf
+cd mbf
+python2 mbf.py
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ] 
+then
+clear
+toilet "Mr.CR45H"
+https://github.com/MrTamfanX/MrDdos
+cd MrDdos
+sh Tamfan-Ddos.sh
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ] 
+then
+clear
+toilet "Mr.CR45H"
+https://github.com/MrTamfanX/MrSadapWhatsApp
+cd MrSadapWhatsApp
+sh MrSadapWhatsApp.sh
+fi
+
+
+
+
